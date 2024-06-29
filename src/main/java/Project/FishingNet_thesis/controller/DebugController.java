f6654f62a9b143c3ce0dd28590711d88fd5d1da8
Carrot_1358 <carrot1358@gmail.com>
2024-06-29 17:26:29 +0700

    fix DebugController

--- src/main/java/Project/FishingNet_thesis/controller/DebugController.java
+++ src/main/java/Project/FishingNet_thesis/controller/DebugController.java
@@ -1,11 +1,13 @@
 package Project.FishingNet_thesis.controller;
 
+import Project.FishingNet_thesis.models.UserDocument;
 import Project.FishingNet_thesis.payload.response.APIResponse;
-import Project.FishingNet_thesis.repository.DefectStatisticsRepository;
-import Project.FishingNet_thesis.repository.FishingDefectRepository;
+import Project.FishingNet_thesis.repository.*;
 import Project.FishingNet_thesis.models.FishingDefectDocument;
 import Project.FishingNet_thesis.payload.debug.Line_request;
 import Project.FishingNet_thesis.payload.debug.RandomDataRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,17 +20,60 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
-import java.util.GregorianCalendar;
 
 @RestController
 @RequestMapping("/debug")
 public class DebugController {
+    private static final Logger logger = LoggerFactory.getLogger(DebugController.class);
     @Autowired
     FishingDefectRepository fishingDefectRepository;
     @Autowired
     DefectStatisticsRepository defectStatisticsRepository;
     @Autowired
+    NotificationController notificationController;
+    @Autowired
+    DeviceRepository deviceRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
     FishingDefectController fishingDefectController;
+
+    public Date randomDate(Date fromDate, Date toDate) {
+        long start = fromDate.getTime();
+        long end = toDate.getTime();
+        long random = start + (long) (Math.random() * (end - start));
+        return new Date(random);
+    }
+
+    public String generateImage(String nameImage, String path, int width, int height) {
+        String directory = System.getProperty("user.dir") + path;
+        String filePath = Paths.get(directory, nameImage + ".jpg").toString();
+        // Create the directory if it does not exist
+        File dir = new File(directory);
+        if (!dir.exists()) {
+            boolean fileCreated = dir.mkdirs();
+            if (!fileCreated) {
+                System.out.println("Can't create directory");
+            } else {
+                System.out.println("Create directory success");
+            }
+        }
+        // Generate a random image
+        String randomImageUrl = "https://picsum.photos/" + width + "/" + height;
+        try {
+            URL url = new URL(randomImageUrl);
+            Path dest = Paths.get(filePath);
+            try (InputStream in = url.openStream()) {
+                Files.copy(in, dest);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filePath;
+    }
+
+    @Autowired
+    private NotificationRepository notificationRepository;
 
     @PostMapping("/removeAll-data-debug")
     public APIResponse removeAllData() {
@@ -38,11 +83,30 @@ public class DebugController {
         File[] files = file.listFiles();
         if (files != null) {
             for (File f : files) {
-                f.delete();
+                boolean delSuccess = f.delete();
+                if (!delSuccess) {
+                    System.out.println("Can't delete file: " + f.getName());
+                }
             }
         }
+        // remove all image in imageProfile
+        File fileProfile = new File(System.getProperty("user.dir") + "/imageProfile");
+        File[] filesProfile = fileProfile.listFiles();
+        if (filesProfile != null) {
+            for (File f : filesProfile) {
+                boolean delSuccess = f.delete();
+                if (!delSuccess) {
+                    System.out.println("Can't delete file: " + f.getName());
+                }
+            }
+        }
+        // remove all data in database
         fishingDefectRepository.deleteAll();
         defectStatisticsRepository.deleteAll();
+        notificationRepository.deleteAll();
+        deviceRepository.deleteAll();
+        userRepository.deleteAll();
+
         res.setStatus(0);
         res.setMessage("Remove all data Success");
         return res;
@@ -52,13 +116,13 @@ public class DebugController {
     public APIResponse randomData(@RequestBody RandomDataRequest randomDataRequest) {
         APIResponse res = new APIResponse();
         Random random = new Random();
-        GregorianCalendar gc = new GregorianCalendar();
         int count = randomDataRequest.getCount();
         while (count > 0) {
             try {
                 String uuid = UUID.randomUUID().toString();
-                String directory = System.getProperty("user.dir") + "/imageDB";
-                String filePath = Paths.get(directory, uuid + ".jpg").toString();
+
+                // Generate a random image
+                /*String filePath = Paths.get(directory, uuid + ".jpg").toString();
 
                 // Create the directory if it does not exist
                 File dir = new File(directory);
@@ -72,10 +136,11 @@ public class DebugController {
                 Path dest = Paths.get(filePath);
                 try (InputStream in = url.openStream()) {
                     Files.copy(in, dest);
-                }
+                }*/
+                String filePath = generateImage(uuid, "/imageDB", 250, 200);
 
                 // Generate a random date
-                // Set the start date (e.g., year 2000)
+                /*// Set the start date (e.g., year 2000)
                 gc.set(randomDataRequest.getYear(),
                         randomDataRequest.getMonth(),
                         randomDataRequest.getDay());
@@ -88,29 +153,44 @@ public class DebugController {
                 long randomDate = startDate + (long) (random.nextDouble() * (endDate - startDate));
                 // Create a new date using the random long value
                 Date randomDateResult = new Date(randomDate);
+
                 System.out.println("Random data ...");
                 System.out.println("Start Date: " + startDate);
                 System.out.println("End Date: " + endDate);
+                System.out.println("Random Date: " + randomDateResult);*/
+                Date randomDateResult = randomDate(randomDataRequest.getFromDate(), new Date());
 
-                System.out.println("Random Date: " + randomDateResult);
                 FishingDefectDocument fishingDefectDocument = new FishingDefectDocument();
                 fishingDefectController.increaseDefectCount(randomDateResult, uuid);
                 fishingDefectDocument.setId(uuid);
                 fishingDefectDocument.setTimestamp(randomDateResult);
                 fishingDefectDocument.setFilename(uuid);
-                fishingDefectDocument.setIsmanaged(random.nextBoolean());
-
-                // Save the file path to the database instead of the Blob
+                boolean isManaged = random.nextBoolean();
+                fishingDefectDocument.setIsmanaged(isManaged);
+                if (isManaged) {
+                    boolean status = random.nextBoolean();
+                    fishingDefectDocument.setStatus(status ? "Activated" : "Deactivated");
+                    try {
+                        fishingDefectDocument.setUserDocument(userRepository.findAll().get(random.nextInt(userRepository.findAll().size())));
+                    } catch (Exception e) {
+                        //create new user
+                        UserDocument userDocument = new UserDocument(UUID.randomUUID().toString(), "user" + random.nextInt(1000), "user" + random.nextInt(1000) + "@gmail.com", "123456");
+                        userRepository.save(userDocument);
+                        fishingDefectDocument.setUserDocument(userDocument);
+                    }
+                }
                 fishingDefectDocument.setImage(filePath);
 
                 fishingDefectRepository.save(fishingDefectDocument);
                 count--;
+                logger.info(fishingDefectDocument.toString());
             } catch (Exception e) {
                 res.setStatus(500);
                 res.setMessage(e.getMessage());
             }
             res.setStatus(0);
             res.setMessage("Random data Success");
+
         }
         return res;
     }
--- src/main/java/Project/FishingNet_thesis/payload/debug/RandomDataRequest.java
+++ src/main/java/Project/FishingNet_thesis/payload/debug/RandomDataRequest.java
@@ -3,6 +3,11 @@ package Project.FishingNet_thesis.payload.debug;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
 @Data
 @Document("random_data_request")
 public class RandomDataRequest {
@@ -10,4 +15,28 @@ public class RandomDataRequest {
     private Integer year;
     private Integer month;
     private Integer day;
+
+    public int defineMonth(){
+        return switch (this.month) {
+            case 1 -> Calendar.JANUARY;
+            case 2 -> Calendar.FEBRUARY;
+            case 3 -> Calendar.MARCH;
+            case 4 -> Calendar.APRIL;
+            case 5 -> Calendar.MAY;
+            case 6 -> Calendar.JUNE;
+            case 7 -> Calendar.JULY;
+            case 8 -> Calendar.AUGUST;
+            case 9 -> Calendar.SEPTEMBER;
+            case 10 -> Calendar.OCTOBER;
+            case 11 -> Calendar.NOVEMBER;
+            case 12 -> Calendar.DECEMBER;
+            default -> throw new IllegalStateException("Unexpected value: " + this.month);
+        };
+    }
+
+
+    public Date getFromDate() {
+        LocalDate localDate = LocalDate.of(year, defineMonth() + 1, day);
+        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
 }
